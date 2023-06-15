0f8b768a5f40f2d0fd84c4ef5a6bd387f9da8309
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-06-15 22:34:12 +0700

    benerin

--- database/seeders/BaseNPMSeeder.php
+++ database/seeders/BaseNPMSeeder.php
@@ -15,6 +15,6 @@ class BaseNPMSeeder extends Seeder
      */
     public function run()
     {
-        BaseNPM::factory()->count(10)->create();
+        BaseNPM::factory()->count(20)->create();
     }
 }
