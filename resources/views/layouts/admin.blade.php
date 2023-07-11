f1dc7be2b5a767d618aaf1b95f9a86f334e5ddab
PutuPersada <putupersada@gmail.com>
2023-07-11 19:58:18 +0700

    Bantuan

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -212,7 +212,7 @@
                             <a class="dropdown-item" href="#"><i class="dw dw-user1"></i> Profil</a>
                         @endif
                         <a class="dropdown-item" href="/settings"><i class="dw dw-settings2"></i> Pengaturan</a>
-                        <a class="dropdown-item" href="/help"><i class="dw dw-help"></i> Bantuan</a>
+                        <a class="dropdown-item" href="/helps"><i class="dw dw-help"></i> Bantuan</a>
                         <a class="dropdown-item" href="/logout"><i class="dw dw-logout"></i> Keluar</a>
                     </div>
                 </div>
