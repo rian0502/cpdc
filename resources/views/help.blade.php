a808708a13d78d0c17b65df7e0986596b8b5160d
PutuPersada <putupersada@gmail.com>
2023-07-12 08:38:10 +0700

    Update

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.umum')
 @section('umum')
-    
+
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            <h1>Bantuan</h1>
+            <p>Ini adalah halaman help</p>
+        </div>
+        </div>
+    </div>
+
 @endsection
