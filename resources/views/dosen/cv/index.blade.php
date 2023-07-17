282876496f9ab1b11adb1bf04028a25f1555c7a3
PutuPersada <putupersada@gmail.com>
2023-07-17 13:05:24 +0700

    Update

--- resources/views/dosen/cv/index.blade.php
+++ resources/views/dosen/cv/index.blade.php
@@ -62,14 +62,14 @@
                             </li>
                             <li>
                                 <h3>Penelitian 1</h3>
-                                <p>sumber dana - jumlah</p>
+                                <p>sumber dana - Jumlah</p>
                             </li>
                             <li>
                                 <p class="date year">2016</p>
                             </li>
                             <li>
                                 <h3>Penelitian 2</h3>
-                                <p>sumber dana - jumlah</p>
+                                <p>sumber dana - Jumlah</p>
                             </li>
                         </ul>
                     </div>
@@ -82,14 +82,14 @@
                             </li>
                             <li>
                                 <h3>Pengabdian 1</h3>
-                                <p>sumber dana - jumlah</p>
+                                <p>sumber dana - Jumlah</p>
                             </li>
                             <li>
                                 <p class="date year">2016</p>
                             </li>
                             <li>
                                 <h3>Pengabdian 2</h3>
-                                <p>sumber dana - jumlah</p>
+                                <p>sumber dana - Jumlah</p>
                             </li>
                         </ul>
                     </div>
