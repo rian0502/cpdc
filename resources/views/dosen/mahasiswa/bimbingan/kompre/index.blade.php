40c52213a69a664dbe2ca90a42b64c5143da6d3c
PutuPersada <putupersada@gmail.com>
2023-07-24 17:38:05 +0700

    Update

--- resources/views/dosen/mahasiswa/bimbingan/kompre/index.blade.php
+++ resources/views/dosen/mahasiswa/bimbingan/kompre/index.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.datatable')
 @section('datatable')
+    <style>
+        .right-button {
+            float: right;
+            margin-top: -25px;
+        }
+
+        /* Gunakan media query untuk mengatur tampilan pada layar berukuran kecil */
+        @media screen and (max-width: 600px) {
+            .right-button {
+                float: none;
+                /* Menghapus float pada layar kecil agar tombol muncul di atas konten */
+                margin-top: 10px;
+                /* Atur margin atas sesuai kebutuhan */
+                text-align: center;
+                /* Pusatkan tombol pada layar kecil */
+            }
+        }
+    </style>
+
     <div class="main-container">
         <div class="pd-ltr-20 xs-pd-20-10">
             <div class="min-height-200px">
                 <div class="card-box mb-30">
                     <div class="pd-20">
                         <h4 class="text-blue h4">Mahasiswa Bimbingan Tugas Akhir</h4>
+                        <a href="" class="btn btn-success right-button"><i class="fa fa-download"></i> Unduh Data</a>
 
                     </div>
                     <div class="pb-20 m-3">
 
-                        <table class="table data-table-responsive stripe data-table-export-excel">
+                        <table class="table data-table-responsive stripe data-table-noexport">
                             <thead>
                                 <tr>
                                     <th>No</th>
@@ -24,11 +44,11 @@
                             <tbody>
                                 @foreach ($seminar as $item)
                                     <tr>
-                                        <td>{{ $loop->iteration}}</td>
-                                        <td>{{ $item->mahasiswa->nama_mahasiswa}}</td>
-                                        <td>{{ $item->mahasiswa->npm}}</td>
-                                        <td>{{ $item->mahasiswa->angkatan}}</td>
-                                        <td>{{ $item->judul_ta}}</td>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->mahasiswa->nama_mahasiswa }}</td>
+                                        <td>{{ $item->mahasiswa->npm }}</td>
+                                        <td>{{ $item->mahasiswa->angkatan }}</td>
+                                        <td>{{ $item->judul_ta }}</td>
                                         <td>
                                             <div class="dropdown">
                                                 <a class="btn btn-outline-primary dropdown-toggle" href="#"
@@ -38,7 +58,7 @@
 
                                                 <div class="dropdown-menu dropdown-menu-right">
                                                     <a class="dropdown-item"
-                                                        href="{{route('dosen.mahasiswa.bimbingan.kompre.show', $item->mahasiswa->npm)}}"><i
+                                                        href="{{ route('dosen.mahasiswa.bimbingan.kompre.show', $item->mahasiswa->npm) }}"><i
                                                             class="fa fa-eye"></i>
                                                         Lihat</a>
 
--- resources/views/dosen/mahasiswa/bimbingan/kp/index.blade.php
+++ resources/views/dosen/mahasiswa/bimbingan/kp/index.blade.php
@@ -1,16 +1,34 @@
 @extends('layouts.datatable')
 @section('datatable')
+    <style>
+        .right-button {
+            float: right;
+            margin-top: -25px;
+        }
+
+        /* Gunakan media query untuk mengatur tampilan pada layar berukuran kecil */
+        @media screen and (max-width: 600px) {
+            .right-button {
+                float: none;
+                /* Menghapus float pada layar kecil agar tombol muncul di atas konten */
+                margin-top: 10px;
+                /* Atur margin atas sesuai kebutuhan */
+                text-align: center;
+                /* Pusatkan tombol pada layar kecil */
+            }
+        }
+    </style>
     <div class="main-container">
         <div class="pd-ltr-20 xs-pd-20-10">
             <div class="min-height-200px">
                 <div class="card-box mb-30">
                     <div class="pd-20">
                         <h4 class="text-blue h4">Mahasiswa Bimbingan PKL/KP</h4>
-
+                        <a href="" class="btn btn-success right-button"><i class="fa fa-download"></i> Unduh Data</a>
                     </div>
                     <div class="pb-20 m-3">
 
-                        <table class="table data-table-responsive stripe data-table-export-excel">
+                        <table class="table data-table-responsive stripe data-table-noexport">
                             <thead>
                                 <tr>
                                     <th>No</th>
@@ -24,30 +42,31 @@
                             </thead>
                             <tbody>
                                 @foreach ($kerja_praktik as $item)
-                                <tr>
-                                    <td>{{ $loop->iteration}}</td>
-                                    <td>{{ $item->mahasiswa->nama_mahasiswa}}</td>
-                                    <td>{{ $item->mahasiswa->npm}}</td>
-                                    <td>{{ $item->judul_kp}}</td>
-                                    <td>{{ $item->mitra}}</td>
-                                    <td>{{ $item->tahun_akademik}}</td>
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->mahasiswa->nama_mahasiswa }}</td>
+                                        <td>{{ $item->mahasiswa->npm }}</td>
+                                        <td>{{ $item->judul_kp }}</td>
+                                        <td>{{ $item->mitra }}</td>
+                                        <td>{{ $item->tahun_akademik }}</td>
 
-                                    <td>
-                                        <div class="dropdown">
-                                            <a class="btn btn-outline-primary dropdown-toggle" href="#" role="button"
-                                                data-toggle="dropdown">
-                                                <i class="fa fa-ellipsis-h"></i>
-                                            </a>
+                                        <td>
+                                            <div class="dropdown">
+                                                <a class="btn btn-outline-primary dropdown-toggle" href="#"
+                                                    role="button" data-toggle="dropdown">
+                                                    <i class="fa fa-ellipsis-h"></i>
+                                                </a>
 
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a class="dropdown-item" href="{{ route('dosen.mahasiswa.bimbingan.kp.show', $item->mahasiswa->npm) }}"><i
-                                                        class="fa fa-eye"></i>
-                                                    Lihat</a>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                    <a class="dropdown-item"
+                                                        href="{{ route('dosen.mahasiswa.bimbingan.kp.show', $item->mahasiswa->npm) }}"><i
+                                                            class="fa fa-eye"></i>
+                                                        Lihat</a>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
+                                        </td>
+                                    </tr>
                                 @endforeach
 
                             </tbody>
