7535fd7d81237e94a1e4baef014253286358d734
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-07-30 03:54:27 +0700

    menambahkan chart untuk seminar dosen

--- routes/web.php
+++ routes/web.php
@@ -131,6 +131,8 @@ Route::get('chart/seminar', [ChartSeminarController::class, 'ChartSeminar'])->na
 Route::get('chart/usiadosen', [AkunDosenController::class, 'chartUsiaDosen'])->name('chart.usia.dosen')->middleware('auth', 'verified', 'role:jurusan');
 Route::get('chart/jabatandosen', [AkunDosenController::class, 'chartJabatanDosen'])->name('chart.jabatan.dosen')->middleware('auth', 'verified', 'role:jurusan');
 Route::get('chart/aktivitasalumni', [AktivitasAlumniController::class, 'chartAktivitasAlumni'])->name('chart.aktivitas.alumni')->middleware('auth', 'verified', 'role:jurusan');
+Route::get('chart/seminardosen', [ControllerSeminarDosen::class, 'chartSeminarDosen'])->name('chart.seminar.dosen')->middleware('auth', 'verified', 'role:jurusan');
+Route::get('chart/tahunseminardosen', [ControllerSeminarDosen::class, 'chartTahunSeminarDosen'])->name('chart.tahunseminar.dosen')->middleware('auth', 'verified', 'role:jurusan');
 // end ADMIN LAB
 
 //admin berkas
