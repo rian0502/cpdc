7b69574c8da3f1b77ffd078438c20c479ac7a6f1
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-02 20:36:35 +0700

    buatkan crud berita acara

---
+++ database/migrations/2023_08_02_203106_template_berita_acara.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TemplateBeritaAcara extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('template_berita_acara', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists ('template_berita_acara');
+    }
+}
