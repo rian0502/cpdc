492d03c10cf9d9aa78632cff952b5b23c33fa32a
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-04 15:28:49 +0700

    tambahin controller ta 2

---
+++ app/Http/Controllers/mahasiswa_s2/ta2/ControllerMahasiswaS2BaTaDua.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\mahasiswa_s2\ta2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControllerMahasiswaS2BaTaDua extends Controller
+{
+    //
+}
