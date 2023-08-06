fa53e57c77090e1da222ae23d878b941a0548263
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-06 15:18:13 +0700

    validasi ta 1 dari sisi admin berkas

--- app/Http/Controllers/mahasiswa_s2/ta1/ControllerAdminS2BpTaSatu.php
+++ app/Http/Controllers/mahasiswa_s2/ta1/ControllerAdminS2BpTaSatu.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\mahasiswa_s2\ta1;
 use App\Http\Controllers\Controller;
 use App\Models\ModelSeminarTaSatuS2;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class ControllerAdminS2BpTaSatu extends Controller
 {
@@ -63,6 +64,12 @@ class ControllerAdminS2BpTaSatu extends Controller
     public function edit($id)
     {
         //
+        $seminar = ModelSeminarTaSatuS2::find(Crypt::decrypt($id));
+        $data = [
+            'seminar' => $seminar,
+            'mahasiswa' => $seminar->mahasiswa,
+        ];
+        return view('admin.admin_berkas.validasi.seminarS2.ta1.edit', $data);
     }
 
     /**
@@ -75,6 +82,26 @@ class ControllerAdminS2BpTaSatu extends Controller
     public function update(Request $request, $id)
     {
         //
+        if($request->status_admin == 'Invalid'){
+            $request->validate([
+                'komentar' => 'required|min:10|string|max:255'
+            ],[
+                'komentar.required' => 'Komentar harus diisi',
+                'komentar.min' => 'Komentar minimal 10 karakter',
+                'komentar.max' => 'Komentar maksimal 255 karakter'
+            ]);
+            $seminar = ModelSeminarTaSatuS2::find(Crypt::decrypt($id));
+            $seminar->status_admin = $request->status_admin;
+            $seminar->komentar = $request->komentar;
+            $seminar->updated_at = date('Y-m-d H:i:s');
+            $seminar->save();
+        }else{
+            $seminar = ModelSeminarTaSatuS2::find(Crypt::decrypt($id));
+            $seminar->status_admin = $request->status_admin;
+            $seminar->updated_at = date('Y-m-d H:i:s');
+            $seminar->save();
+        }
+        return redirect()->route('berkas.validasi.s2.tesis1.index')->with('success', 'Berhasil Mengubah data');
     }
 
     /**
