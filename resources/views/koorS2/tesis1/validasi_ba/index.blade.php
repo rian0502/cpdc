966c2298ade74ca8fb1b400a3d181c355eac009e
PutuPersada <putupersada@gmail.com>
2023-08-10 08:35:51 +0700

    Update

--- resources/views/koorS2/tesis1/validasi_ba/index.blade.php
+++ resources/views/koorS2/tesis1/validasi_ba/index.blade.php
@@ -5,7 +5,7 @@
             <div class="min-height-200px">
                 <div class="card-box mb-30">
                     <div class="pd-20">
-                        <h4 class="text-blue h4">Validasi Berita Acara Seminar Tugas Akhir 1</h4>
+                        <h4 class="text-blue h4">Validasi Berita Acara Seminar Tugas Akhir 1 S2</h4>
                     </div>
                     <div class="pb-20 m-3">
 
@@ -15,7 +15,7 @@
                                     <th>No</th>
                                     <th>NPM</th>
                                     <th>Judul</th>
-                                    <th>No Surat</th>
+                                    <th>Nomor Surat</th>
                                     <th>Status</th>
                                     <th class="table-plus datatable-nosort">Aksi</th>
                                 </tr>
