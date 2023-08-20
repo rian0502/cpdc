66f8cfeee56aff1694d3c203a335a097e868c801
PutuPersada <putupersada@gmail.com>
2023-08-20 21:57:02 +0700

    Update

--- resources/views/koorS2/tesis2/jadwal/index.blade.php
+++ resources/views/koorS2/tesis2/jadwal/index.blade.php
@@ -5,17 +5,7 @@
             <div class="min-height-200px">
                 <div class="card-box mb-30">
                     <div class="pd-20">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        <h4 class="text-blue h4">Penjadwalan Seminar Tugas Akhir 2 S2</h4>
-=======
                         <h4 class="text-blue h4">Penjadwalan Seminar Tesis 2</h4>
->>>>>>> a47b569c98fe0f348dde85a77f67ecd20d911834
-=======
-
-                        <h4 class="text-blue h4">Penjadwalan Seminar Tesis 2</h4>
-
->>>>>>> 7f72bce0c799fb9c87df96121dacce867c3fa446
                     </div>
                     <div class="pb-20 m-3">
 
@@ -34,69 +24,68 @@
                             </thead>
                             <tbody>
                                 @foreach ($seminar as $item)
-                                   
-                                        <tr>
+                                    <tr>
+                                        <td>
+                                            {{ $loop->iteration }}
+                                        </td>
+                                        <td>
+                                            {{ $item->mahasiswa->npm }}
+                                        </td>
+                                        <td>
+                                            {{ \Illuminate\Support\Str::limit($item->judul_ta, $limit = 40, $end = '...') }}
+                                        </td>
+                                        <td>
+                                            {{ $item->periode_seminar }}
+                                        </td>
+                                        @if ($item->jadwal)
                                             <td>
-                                                {{ $loop->iteration }}
+                                                {{ $item->jadwal->lokasi->nama_lokasi }}
                                             </td>
                                             <td>
-                                                {{ $item->mahasiswa->npm }}
+                                                {{ $carbon::parse($item->jadwal->tanggal)->format('d F Y') }}
                                             </td>
                                             <td>
-                                                {{ \Illuminate\Support\Str::limit($item->judul_ta, $limit = 40, $end = '...') }}
+                                                {{ $item->jadwal->jam_mulai . ' - ' . $item->jadwal->jam_selesai }}
+                                            </td>
+                                        @else
+                                            <td>
+                                                <span class="badge bg-warning">Belum Terjadwal</span>
                                             </td>
                                             <td>
-                                                {{ $item->periode_seminar }}
+                                                <span class="badge bg-warning">Belum Terjadwal</span>
                                             </td>
-                                            @if ($item->jadwal)
-                                                <td>
-                                                    {{ $item->jadwal->lokasi->nama_lokasi }}
-                                                </td>
-                                                <td>
-                                                    {{ $carbon::parse($item->jadwal->tanggal)->format('d F Y') }}
-                                                </td>
-                                                <td>
-                                                    {{ $item->jadwal->jam_mulai . ' - ' . $item->jadwal->jam_selesai }}
-                                                </td>
-                                            @else
-                                                <td>
-                                                    <span class="badge bg-warning">Belum Terjadwal</span>
-                                                </td>
-                                                <td>
-                                                    <span class="badge bg-warning">Belum Terjadwal</span>
-                                                </td>
-                                                <td>
-                                                    <span class="badge bg-warning">Belum Terjadwal</span>
-                                                </td>
-                                            @endif
+                                            <td>
+                                                <span class="badge bg-warning">Belum Terjadwal</span>
+                                            </td>
+                                        @endif
 
-                                            <td>
-                                                <div class="dropdown">
-                                                    <a class="btn btn-outline-primary dropdown-toggle" href="#"
-                                                        role="button" data-toggle="dropdown">
-                                                        <i class="fa fa-ellipsis-h"></i>
-                                                    </a>
-                                                    <div class="dropdown-menu dropdown-menu-right">
-                                                        @if ($item->jadwal)
-                                                            <a class="dropdown-item"
-                                                                href="{{ route('koor.jadwalTA2S2.edit', $item->encrypt_id) }}"><i
-                                                                    class="fa fa-pencil"></i>
-                                                                Edit</a>
-                                                                <a class="dropdown-item"
-                                                                href="{{ route('koor.jadwalTA2S2.resend', $item->encrypt_id) }}"><i
-                                                                    class="fa fa-share"></i>
-                                                                Kirim Kembali</a>
-                                                        @else
-                                                            <a class="dropdown-item"
-                                                                href="{{ route('koor.jadwalTA2S2.create', $item->encrypt_id) }}"><i
-                                                                    class="bi bi-calendar-plus-fill"></i>
-                                                                Jadwalkan</a>
-                                                        @endif
-                                                    </div>
+                                        <td>
+                                            <div class="dropdown">
+                                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
+                                                    data-color="#1b3133" href="#" role="button"
+                                                    data-toggle="dropdown">
+                                                    <i class="dw dw-more"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                                     @if ($item->jadwal)
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('koor.jadwalTA2S2.edit', $item->encrypt_id) }}"><i
+                                                                class="dw dw-edit2"></i>
+                                                            Edit</a>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('koor.jadwalTA2S2.resend', $item->encrypt_id) }}"><i
+                                                                class="fa fa-share"></i>
+                                                            Kirim Kembali</a>
+                                                    @else
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('koor.jadwalTA2S2.create', $item->encrypt_id) }}"><i
+                                                                class="bi bi-calendar-plus-fill"></i>
+                                                            Jadwalkan</a>
+                                                    @endif
                                                 </div>
-                                            </td>
-                                        </tr>
-                      
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
