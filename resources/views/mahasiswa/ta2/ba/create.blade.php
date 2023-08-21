c5072e222a5d17825f3176f15f58188528c318f3
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2023-08-21 13:06:40 +0700

    benerin name ta2

--- resources/views/mahasiswa/ta2/ba/create.blade.php
+++ resources/views/mahasiswa/ta2/ba/create.blade.php
@@ -85,18 +85,18 @@
                             <div class="kanan weight-500 col-md-6">
 
                                 <div class="form-group">
-                                    <label> Berkas Berita Acara<small> <a id="link-ba_seminar_ta_dua" href="#"
+                                    <label> Berkas Berita Acara<small> <a id="link-berkas_ba_seminar_ta_dua" href="#"
                                                 target="_blank" style="display: none;">Lihat File</a> </small></label>
                                     <div class="custom-file">
-                                        <label class="custom-file-label" for="link-ba_seminar_ta_dua"
-                                            id="label-ba_seminar_ta_dua">Pilih File</label>
-                                        <input value="{{ old('ba_seminar_ta_dua') }}" accept=".pdf" autofocus
-                                            name="ba_seminar_ta_dua" id="file-ba_seminar_ta_dua"
-                                            class="custom-file-input form-control @error('ba_seminar_ta_dua') form-control-danger @enderror"
+                                        <label class="custom-file-label" for="link-berkas_ba_seminar_ta_dua"
+                                            id="label-berkas_ba_seminar_ta_dua">Pilih File</label>
+                                        <input value="{{ old('berkas_ba_seminar_ta_dua') }}" accept=".pdf" autofocus
+                                            name="berkas_ba_seminar_ta_dua" id="file-berkas_ba_seminar_ta_dua"
+                                            class="custom-file-input form-control @error('berkas_ba_seminar_ta_dua') form-control-danger @enderror"
                                             type="file" placeholder="FILE SK"
-                                            onchange="updateFileNameAndLink('file-ba_seminar_ta_dua','label-ba_seminar_ta_dua','link-ba_seminar_ta_dua')">
+                                            onchange="updateFileNameAndLink('file-berkas_ba_seminar_ta_dua','label-berkas_ba_seminar_ta_dua','link-berkas_ba_seminar_ta_dua')">
                                     </div>
-                                    @error('ba_seminar_ta_dua')
+                                    @error('berkas_ba_seminar_ta_dua')
                                         <div class="form-control-feedback has-danger mt-2">{{ $message }}</div>
                                     @enderror
                                 </div>
