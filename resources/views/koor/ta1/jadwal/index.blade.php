dc0f1a9476d712ca59b493a23be8caee92086efc
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2023-10-02 16:21:09 +0700

    tombol download data jadwal ta1 yang belum terjadwal

--- resources/views/koor/ta1/jadwal/index.blade.php
+++ resources/views/koor/ta1/jadwal/index.blade.php
@@ -8,7 +8,22 @@
                         <h4 class="text-blue h4">Penjadwalan Seminar Tugas Akhir 1</h4>
                     </div>
                     <div class="pb-20 m-3">
-
+                        <div class="ml-3">
+                            <form action="{{-- route('jurusan.unduh.mahasiswa.seminar') --}}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="weight-500 col-md-3">
+                                        <div class="form-group">
+                                            <div class="cta  d-flex align-items-center justify-content-start"
+                                                style="margin-top: 34px">
+                                                <button class="btn btn-sm btn-success"><i class="fa fa-download"></i>
+                                                    Unduh Detail Data</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                         <table class="table data-table-responsive stripe data-table-export">
                             <thead>
                                 <tr>
