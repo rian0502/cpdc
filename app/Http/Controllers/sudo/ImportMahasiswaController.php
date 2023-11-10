4284888120f059651b3aea7ea0e3fbce33cd84a7
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-11-10 17:33:05 +0700

    menambahkan controller

--- app/Http/Controllers/sudo/ImportMahasiswaController.php
+++ app/Http/Controllers/sudo/ImportMahasiswaController.php
@@ -11,6 +11,8 @@ class ImportMahasiswaController extends Controller
     public function index(){
         return view('sudo.import.mahasiswa');
     }
+
+    
     public function store(Request $request){
         return dd($request->all());
     }
